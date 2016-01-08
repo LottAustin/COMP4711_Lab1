ebd284ec314138e821800ad5ed4df445252c5606
Austin Lott <ark.sky.dragon@gmail.com>
2016-01-07 16:48:35 -0800

    Step 5

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
     </head>
     <body>
         <?php
-            echo 'hello world';
+            $temp = 'Jim';
+            echo 'Hi, my name is';
+            echo $temp;
+            $temp = 'geek';
+            echo 'I am a';
+            echo $temp;
+            $temp = 10;
+            echo 'My level is';
+            echo $temp;
         ?>
     </body>
 </html>
