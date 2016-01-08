64a8ad386a72e3aac9bef6206ae5c04530f08041
Austin Lott <ark.sky.dragon@gmail.com>
2016-01-08 11:58:07 -0800

    PHP iteration

--- index.php
+++ index.php
@@ -18,39 +18,32 @@
 
 <?php       
             function winner($token,$position) {
-            $won = false;
-            if (($position[0] == $token) &&
-            ($position[1] == $token) &&
-            ($position[2] == $token)) {
-                $won = true;
-            } else if (($position[3] == $token) &&
-            ($position[4] == $token) &&
-            ($position[5] == $token)) {
-                $won = true;
-            } else if(($position[6] == $token) &&
-            ($position[7] == $token) &&
-            ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
-            ($position[3] == $token) &&
-            ($position[6] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-            ($position[4] == $token) &&
-            ($position[7] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-            ($position[5] == $token) &&
-            ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
-            ($position[4] == $token) &&
-            ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-            ($position[4] == $token) &&
-            ($position[6] == $token)) {
-                $won = true;
-            }
-            return $won;
+                $won = false;
+                for($row=0; $row<3; $row++) {
+                    $won = true;
+                    for($col=0; $col<3; $col++) {
+                        if ($position[3*$row+$col] != $token) { $won = false; }
+                        //note the negative test
+                    }
+                    if($won == true) {
+                        return $won;
+                    }
+                    $won = true;
+                    for($col=0; $col<3; $col++) {
+                        if ($position[3*$col+$row] != $token) { $won = false; }
+                    } 
+                    if($won == true) {
+                        return $won;
+                    }
+                }
+                if(($position[0] == $token) &&
+                ($position[4] == $token) &&
+                ($position[8] == $token)) {
+                    $won = true;
+                } else if(($position[0] == $token) &&
+                ($position[4] == $token) &&
+                ($position[8] == $token)) {
+                    $won = true;
+                }
+                return $won;
             }
