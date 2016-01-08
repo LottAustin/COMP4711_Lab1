4800750cbb1d2feb5d2feb850dc21c4b815c0178
Austin Lott <ark.sky.dragon@gmail.com>
2016-01-07 16:59:46 -0800

    php selection

--- index.php
+++ index.php
@@ -11,11 +11,15 @@
             $level = 10;
             echo 'Hi, my name is '.$name,'. and I am a level '.$level.'
             '.$what;
-            echo '<br/>';
             $hoursworked = 10;
             $rate = 12;
+            if ($hoursworked > 40) {
+            $total = $hoursworked * $rate * 1.5;
+            } else {
             $total = $hoursworked * $rate;
-            echo 'You owe me '.$total;
+            }
+            echo '<br/>';
+            echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
         ?>
     </body>
 </html>
